12cefe4d06bf975273e2e1480c73543c85002835
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2024-01-13 15:39:55 -0300

    separate styled component

---
+++ src/components/Text/InfoDetatails/style.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  ViewAreaText: {
+    alignItems: "left",
+    marginLeft: 10,
+    backgroundColor: "#E2E1E7",
+    borderRadius: 10,
+  },
+  ViewText: {
+    marginLeft: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 10,
+  },
+  Text: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  TitleText: {
+    marginTop: 30,
+    marginBottom: 30,
+    textAlign: "center",
+    fontSize: 30,
+  },
+});
+
+export default styles;
